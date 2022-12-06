99b5af627be5d15d82428192129cf99b68c66290
Miguel Nazareth <miguelnazareth22@gmail.com>
2022-12-06 15:16:00 -0600

    Eliminacion de Django CRUD

--- Django Crud/tasks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-class TaskAdmin(admin.ModelAdmin):
-    # Cuales campos solo son de lectura y que se vean en la pantalla
-    readonly_fields = ("created",)
-
-# Register your models here.
-admin.site.register(Task, TaskAdmin)
